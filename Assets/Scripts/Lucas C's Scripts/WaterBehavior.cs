0947588643c495129830fed4aeba2946284d3596
AstralOnion <139502233+AstralOnion@users.noreply.github.com>
2026-04-06 16:34:45 -0400

    Update WaterBehavior.cs

--- Assets/Scripts/Lucas C's Scripts/WaterBehavior.cs
+++ Assets/Scripts/Lucas C's Scripts/WaterBehavior.cs
@@ -29,7 +29,7 @@ public class WaterBehavior : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            //playerController.CanJump = true;
+            //playerController.CanJump = false;
             //playerController.speed = playerController.speed * underwaterSpeedMultiplier;
         }
     }
@@ -37,7 +37,7 @@ public class WaterBehavior : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            //playerController.CanJump = false;
+            //playerController.CanJump = true;
             //playerController.speed = originalPlayerSpeed;
         }
     }
