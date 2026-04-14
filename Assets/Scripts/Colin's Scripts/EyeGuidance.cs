22df3bec44b1ff0408a69e070c449a0421cffd86
Snadicus <iscreamatrats@gmail.com>
2026-04-14 17:02:16 -0400

    Small Update

--- Assets/Scripts/Colin's Scripts/EyeGuidance.cs
+++ Assets/Scripts/Colin's Scripts/EyeGuidance.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class EyeGuidance : MonoBehaviour
 {
-    
+    // Variables & References
+    #region
+
+    Collider sunCollider;
+    Collider eyeCollider;
+    Collider mouthCollider;
+
+    #endregion
+
+    // Start
+    #region
+
+    // Grab each Component needed for 'animation'
+    void Start()
+    {
+        //sunCollider = GetComponent
+
+    }
+
+    #endregion
 }
