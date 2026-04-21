548c6c955ebb28e970c63f4b78c5bb68022f4b18
tiptonblu3 <tiptonblu3@gmail.com>
2026-04-21 01:10:50 -0400

    Fixes again

--- Assets/Scripts/Freddie's Scripts/SaveProgress.cs
+++ Assets/Scripts/Freddie's Scripts/SaveProgress.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class SaveProgress : MonoBehaviour
 {
 
-    string sceneName = SceneManager.GetActiveScene().name;
-    string path = Path.Combine(Application.persistentDataPath, sceneName + "_Save.xml");
+    //string sceneName = SceneManager.GetActiveScene().name;
+    //string path = Path.Combine(Application.persistentDataPath, sceneName + "_Save.xml");
 
     //public void
-     
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
