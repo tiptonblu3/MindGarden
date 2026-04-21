edc471900e4be83dfc10d880c4587c6809225b07
tiptonblu3 <tiptonblu3@gmail.com>
2026-04-21 01:08:42 -0400

    Saves CheckPoints and RecordPieces

---
+++ Assets/Scripts/Freddie's Scripts/RecordPiece.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class RecordPiece : MonoBehaviour, IInteractable
+{
+    public CheckPoints CheckPoints;
+
+    public void Interact()
+    {
+        Debug.Log("Record Piece Picked Up!");
+        CheckPoints.CurrentCheckPointIndex++; // Increment the check point index
+        gameObject.SetActive(false); // This will disable the record object
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
