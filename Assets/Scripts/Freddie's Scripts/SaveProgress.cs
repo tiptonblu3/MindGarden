8e3942c38b7a344b6707dc59d871e8be1fb459b1
tiptonblu3 <tiptonblu3@gmail.com>
2026-04-21 01:09:58 -0400

    Save Reset

--- Assets/Scripts/Freddie's Scripts/SaveProgress.cs
+++ Assets/Scripts/Freddie's Scripts/SaveProgress.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class SaveProgress : MonoBehaviour
 {
+
+    string sceneName = SceneManager.GetActiveScene().name;
+    string path = Path.Combine(Application.persistentDataPath, sceneName + "_Save.xml");
+
+    //public void
+     
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
