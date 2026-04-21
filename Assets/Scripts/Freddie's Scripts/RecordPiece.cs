a6d7ce92f32e2a9738e3bd37ebbeb7db7b72612d
tiptonblu3 <tiptonblu3@gmail.com>
2026-04-21 03:10:11 -0400

    Messy Save things
    Not working Completely

--- Assets/Scripts/Freddie's Scripts/RecordPiece.cs
+++ Assets/Scripts/Freddie's Scripts/RecordPiece.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 public class RecordPiece : MonoBehaviour, IInteractable
 {
     public CheckPoints CheckPoints;
+    public GameObject SaveProgressObject;
+    private SaveProgress saveProgress;
+
+    void Awake()
+    {
+        CheckPoints = GameObject.FindGameObjectWithTag("Checkpoints").GetComponent<CheckPoints>();
+        SaveProgressObject = GameObject.FindGameObjectWithTag("SaveProgress");
+        saveProgress = SaveProgressObject.GetComponent<SaveProgress>();
+    }
+
 
     public void Interact()
     {
         Debug.Log("Record Piece Picked Up!");
         CheckPoints.CurrentCheckPointIndex++; // Increment the check point index
+        saveProgress.SaveGame(); // Save the progress
         gameObject.SetActive(false); // This will disable the record object
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
