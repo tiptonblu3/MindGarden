eb0fe6a26111f0a31851f9ff013cff89a234fb3e
Jordon N Dubin <jordon.n.dubin@gmail.com>
2026-04-26 17:09:35 -0400

    Jermey can do the rest
    I fixed the video

--- Assets/Scripts/Jordon's Scripts/Disco Mechanic/KillZone.cs
+++ Assets/Scripts/Jordon's Scripts/Disco Mechanic/KillZone.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class KillZone : MonoBehaviour
 {
     // Drag your Respawn Point object into this slot in the Inspector
-    public Transform respawnPoint;
-    public PlatformManager PlatManScript;
+    public Player_Movement playermove;
     
 
     private void OnTriggerEnter(Collider other)
@@ -12,20 +11,7 @@ public class KillZone : MonoBehaviour
         // 1. Check if the thing hitting the zone is the Player
         if (other.CompareTag("Player"))
         {
-            if (respawnPoint != null)
-            {
-                // 2. Move the Player (other) to the respawn point
-                other.transform.position = respawnPoint.position;
-
-                // 3. Stop physics momentum so they don't keep falling speed
-                Rigidbody rb = other.GetComponent<Rigidbody>();
-                if (rb != null)
-                {
-                    rb.linearVelocity = Vector3.zero;
-                    rb.angularVelocity = Vector3.zero;
-                }
-            PlatManScript.ResetAllPlatforms();
-            }
+            playermove.isDead = true; // Set the player's isDead flag to true
         }
     }
 }
