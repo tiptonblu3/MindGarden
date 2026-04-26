b7f0d41a3096f603d3e09e10bebd26b488e38d60
tiptonblu3 <tiptonblu3@gmail.com>
2026-04-26 00:29:45 -0400

    CheckPoint Fixes

--- Assets/Scripts/Freddie's Scripts/RecordPiece.cs
+++ Assets/Scripts/Freddie's Scripts/RecordPiece.cs
@@ -2,41 +2,32 @@ using UnityEngine;
 
 public class RecordPiece : MonoBehaviour, IInteractable
 {
-    public bool IsFloating = true; // Option to enable or disable floating animation
-    private Vector3 startPos; // Store the initial position of the record for animation purposes
     public CheckPoints CheckPoints;
-    public GameObject SaveProgressObject;
-    private SaveProgress saveProgress;
 
     void Awake()
     {
         CheckPoints = GameObject.FindGameObjectWithTag("Checkpoints").GetComponent<CheckPoints>();
-        SaveProgressObject = GameObject.FindGameObjectWithTag("SaveProgress");
-        saveProgress = SaveProgressObject.GetComponent<SaveProgress>();
     }
-
 
     public void Interact()
     {
-        Debug.Log("Record Piece Picked Up!");
-        CheckPoints.CurrentCheckPointIndex++; // Increment the check point index
-        saveProgress.SaveGame(); // Save the progress
-        gameObject.SetActive(false); // This will disable the record object
+        if (gameObject.activeInHierarchy)
+        {
+            Debug.Log("Record Piece Picked Up!");
+            CheckPoints.CurrentCheckPointIndex++; // Increment the check point index
+            gameObject.SetActive(false); // This will disable the record object
+        }
+        
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        startPos = transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsFloating)
-        {
-            transform.Rotate(Vector3.up, 50 * Time.deltaTime);
-            float bounce = Mathf.Sin(Time.time * 2) * 0.1f;
-            transform.position = new Vector3(startPos.x, startPos.y + bounce, startPos.z);
-        }
+        
     }
 }
