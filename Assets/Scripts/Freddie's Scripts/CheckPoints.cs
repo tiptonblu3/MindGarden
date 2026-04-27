28a8fa99d48f0faed6981aa01b35e7c4727ba5aa
tiptonblu3 <tiptonblu3@gmail.com>
2026-04-27 02:10:04 -0400

    Saving the Game
    Next is making the discs know when to disable based on checkpointindex
    also resetting levels so that checkpoints will = 0 again

--- Assets/Scripts/Freddie's Scripts/CheckPoints.cs
+++ Assets/Scripts/Freddie's Scripts/CheckPoints.cs
@@ -12,11 +12,15 @@ public class CheckPoints : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
+    }
+
+    void Awake()
+    {
         // Find the player by tag
         Player = GameObject.FindGameObjectWithTag("Player");
         PlayerTransform = Player.transform;
         PlayerMovement = Player.GetComponent<Player_Movement>();
-        Spawn(); // Move the player to the first checkpoint at the start of the game
     }
 
     // Update is called once per frame
@@ -32,22 +36,14 @@ public class CheckPoints : MonoBehaviour
 
     public void Respawn()
     {
-        // Move the player to the position of the current checkpoint
+        if (PlayerTransform == null) return; // Safety check
+        // Move the player
         PlayerTransform.position = CheckPointList[CurrentCheckPointIndex].transform.position;
+        
+        Debug.Log("Player Respawned to index: " + CurrentCheckPointIndex);
     }
     public void ResetCheckpoints()
     {
         CurrentCheckPointIndex = 0; // Reset to the first checkpoint
-    }
-    System.Collections.IEnumerator InitialRespawn()
-    {
-        // Wait until the end of the first frame so physics/cameras are settled
-        yield return new WaitForEndOfFrame();
-        Respawn();
-    }
-
-    public void Spawn()
-    {
-        StartCoroutine(InitialRespawn());
     }
 }
