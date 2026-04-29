db25f1a26877b9ebbedfbfa70dad7d6bf838ca18
Jordon N Dubin <jordon.n.dubin@gmail.com>
2026-04-29 00:33:57 -0400

    Augmented audio
    raised volume so that for customization it will be hearable at any volume
    and made puzzle completely random every time

--- Assets/Art Assets/JeremyLevel/Scripts/StudioEndTrigger.cs
+++ Assets/Art Assets/JeremyLevel/Scripts/StudioEndTrigger.cs
@@ -5,10 +5,12 @@ public class StudioEndTrigger : MonoBehaviour
     public NighmarManager nightmar;
     public GameObject Platforms;
     public GameObject Bridge;
-
+    public PlatformManager platmanscript;
 
     private void OnTriggerEnter(Collider other)
     {
+        
+        platmanscript.musicSource.Stop();
         nightmar.isNighmarActive = true;
         Platforms.SetActive(false);
         Bridge.SetActive(true);
